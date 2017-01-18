b5b127567066bfe0143ab8c782e0ca0876686d83
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-01-18 18:20:17 +0500

    New functionality added for adding a person

--- src/java/helloworld/PeopleBean.java
+++ src/java/helloworld/PeopleBean.java
@@ -5,7 +5,12 @@
  */
 package helloworld;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Singleton;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import models.Person;
 
 /**
  *
@@ -21,5 +26,31 @@ public class PeopleBean {
 
     public void setNames(String[] names) {
         this.names = names;
+    }
+    
+    
+    
+    
+    List<Person> people = new ArrayList<Person>();
+    
+    public List<Person> getPeople(){
+        if (people.size() <= 0) {
+            people.add(new Person("Bob", 21));
+            people.add(new Person("Tom", 21));
+            people.add(new Person("Jack", 21));
+            people.add(new Person("Jill", 21));
+            people.add(new Person("Jessie", 21));
+            people.add(new Person("Tabor", 21));
+            people.add(new Person("William", 21));
+        }
+        
+        return people;
+    }
+    
+    
+    public void AddPerson(String name, int age){
+        if (people != null) {
+            people.add(new Person(name, age));
+        }
     }
 }
